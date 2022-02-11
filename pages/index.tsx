f954e1466facff6ddd25ac8c32cc94e7bdad531e
FIN443 <jjhun1228@gmail.com>
2022-02-11 14:56:22 +0900

    style & api route

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 import { useQuery } from "react-query";
 
-const API_KEY = "96267eb5d8531873d025017bde38767c";
-
 interface IGetMoviesProps {
   page: number;
   results: IMovieProps[];
@@ -23,9 +21,7 @@ interface IMovieProps {
 }
 
 function getMovies() {
-  return fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-  ).then((response) => response.json());
+  return fetch(`/api/movies`).then((response) => response.json());
 }
 
 export default function Home() {
@@ -35,17 +31,42 @@ export default function Home() {
   );
 
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
       {isLoading ? (
         <h4>Loading...</h4>
       ) : (
         data?.results.map((movie) => (
-          <div key={movie.id}>
+          <div className="movie" key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
             <h4>{movie.original_title}</h4>
           </div>
         ))
       )}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
